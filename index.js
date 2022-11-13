8472cf2e4b3083a03d97e72f5677bbab10881dca
jokatty <jyotikattani@gmail.com>
2022-11-13 16:57:26 +0530

    add view engine to serve index file

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+// set view engine
+app.set('view engine', 'ejs');
 // set root route
 app.get('/', (req, res) => {
-  res.send('hi');
+  res.render('index');
 });
 
 // start the server at port 3000
